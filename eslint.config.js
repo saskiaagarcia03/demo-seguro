67bf4b0356ebb14119173e0040b122491133a31c
saskiaagarcia03 <sagarciaza@uide.edu.ec>
2025-04-07 15:38:04 -0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,20 @@
 // eslint.config.js
+const securityPlugin = require("eslint-plugin-security");
+
 module.exports = [
   {
     files: ["**/*.js"],
     languageOptions: {
-      globals: { node: true }, // Equivalente a "env": {"node": true}
+      globals: { node: true },
+    },
+    plugins: {
+      security: securityPlugin,
     },
     rules: {
       "semi": ["error", "always"],
+      "security/detect-eval-with-expression": "error",
+      "security/detect-new-function": "warn",
+      "security/detect-object-injection": "warn",
     },
   },
 ];
