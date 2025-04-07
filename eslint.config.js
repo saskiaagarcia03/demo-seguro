f4cd8f66d065815c2657e1b83218dde12b1dc5f6
“saskiaagarcia03” <sagarciaza@uide.edu.ec>
2025-04-07 16:28:43 -0500

    Actualización de ESLint y configuración de reglas --

--- eslint.config.js
+++ eslint.config.js
@@ -3,19 +3,16 @@ const securityPlugin = require("eslint-plugin-security");
 module.exports = [
   {
     files: ["**/*.js"],
-    languageOptions: {
-      globals: { node: true },
-    },
     plugins: {
       security: securityPlugin,
     },
     rules: {
       "semi": ["error", "always"],
-      "security/detect-eval-with-expression": "error",  // Detecta el uso de eval
-      "security/detect-object-injection": "warn",       // Detecta inyecciones de objetos
+      "security/detect-eval-with-expression": "error", // Marca el uso de eval()
     },
   },
 ];
+
 
 
 
