560f72bcbd8be746526bc96114c4348c47fb6a53
“saskiaagarcia03” <sagarciaza@uide.edu.ec>
2025-04-11 09:55:06 -0500

    Ejecución SAST

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,22 @@
 import securityPlugin from 'eslint-plugin-security';
 
-export default [
-  {
-    files: ["**/*.js"],
-    plugins: {
-      security: securityPlugin,
-    },
-    rules: {
-      "semi": ["error", "always"],
-      "quotes": ["error", "double"],
-      "security/detect-eval-with-expression": "error",
-      "security/detect-new-buffer": "error",
-      "security/detect-non-literal-require": "error",
-      "security/detect-object-injection": "error"
-    },
+export default {
+  plugins: {
+    security: securityPlugin,
   },
-];
-
-
+  rules: {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "security/detect-eval-with-expression": "error",
+    "security/detect-new-buffer": "error",
+    "security/detect-non-literal-require": "error",
+    "security/detect-object-injection": "error"
+  },
+  parserOptions: {
+    ecmaVersion: 2020,  // O usa "latest" si prefieres la versión más reciente de ECMAScript
+    sourceType: "module"
+  }
+};
 
 
 
